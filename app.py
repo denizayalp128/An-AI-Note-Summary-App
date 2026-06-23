f6dae49359b2ca923eee5966f64b011795ff3c1e
denizayalp128 <denizayalp128@gmail.com>
2026-06-23 22:03:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        notes = request.form["notes"]
+        return notes
+
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
