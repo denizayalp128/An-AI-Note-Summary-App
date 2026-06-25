95cf247a772306252611131aab304504e12bf455
denizayalp128 <denizayalp128@gmail.com>
2026-06-25 12:29:23 +0300

    Modify home function to include notes length
    Update home function to pass notes and its length to the template.

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+
+    notes = ""
+
     if request.method == "POST":
         notes = request.form["notes"]
-        return notes
 
-    return render_template("index.html")
-
+    return render_template("index.html", notes=notes, lenghtofnotes=len(notes))
 if __name__ == "__main__":
     app.run(debug=True)
